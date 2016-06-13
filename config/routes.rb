ecae9542152b68b0cf6459073edebe38bcf81e52
Anya <anknya@gmail.com>
2016-06-12 23:08:57 -0700

    Created Home and About pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
-Rails.application.routes.draw do
+#Rails.application.routes.draw do
+ Pintresting::Application.routes.draw do
+  root "pages#home"
+  #replaced get 'pages/home'
+  get "about" => "pages#about" #creates about_path
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
